ff58b0a8f3d1baae9b4840bf8da288b57ded017f
Pedro Cisneros Santana <pedro.cisneros.santana@gmail.com>
2019-11-28 01:13:32 +0100

    Update Ejercicio1.sql

--- Ejercicio1.sql
+++ Ejercicio1.sql
@@ -2,7 +2,6 @@
  * Hallar por orden alfabético los nombres de los departamentos cuyo director
  * lo es en funciones y no en propiedad.
  */
---SELECT * FROM TDEPTO t
 SELECT NOMEM FROM TEMPLE t
 WHERE NUMDE IN (select NUMDE FROM TDEPTO WHERE TIDIR='F')
 ORDER BY NOMEM
